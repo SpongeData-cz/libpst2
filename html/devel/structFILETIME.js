3d00baa628764dc64bc529b56e5dcb132bef21ff
mafiosso <pavelvonlostice@seznam.cz>
2023-09-19 10:19:55 +0200

    Get rid of generated binaries and doc.

--- html/devel/structFILETIME.js
+++
@@ -1,5 +0,0 @@
-var structFILETIME =
-[
-    [ "dwHighDateTime", "structFILETIME.html#a543da86c41405dab23039927ef7bc5bf", null ],
-    [ "dwLowDateTime", "structFILETIME.html#a9eb75a091d37f541085bd2cb6607651b", null ]
-];
